80a7533a28b7506fa5f014378fe4dedb15e347a8
MaxFerrarottiDEV <maximo_ferrarotti@hotmail.com>
2024-10-02 16:23:23 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+
+import saludo from "./assets/saludo.jpg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="light" />
+      <Image
+        source={saludo}
+        style={{
+          width: 100,
+          height: 100,
+          resizeMode: "center",
+        }}
+      />
+      <Text>Hola, Bienvenido a mi primera APP</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Boton Nativo - Pulsa aquí"
+        onPress={() => alert("Funcion en progreso...")}
+      />
     </View>
   );
 }
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
