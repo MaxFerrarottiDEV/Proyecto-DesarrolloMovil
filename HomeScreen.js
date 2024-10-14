464c3ea116012d386f2b4b00534dd2776a8d5e0a
MaxFerrarottiDEV <maximo_ferrarotti@hotmail.com>
2024-10-14 17:14:39 -0300

    Actualizacion pequeña para las todas las interfaces en gral

--- HomeScreen.js
+++ HomeScreen.js
@@ -14,10 +14,18 @@ export default function HomeScreen({ navigation }) {
         style={styles.image}  // Estilos para la imagen
       />
       
-      <Button
-        title="Ir a Materias"
-        onPress={() => navigation.navigate("Materias")}
-      />
+      {/* Carta de presentación */}
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Inscripciones:</Text>
+        <Text style={styles.cardText}>
+          Permite gestionar solicitudes de inscripción de nuevos candidatos y revisar o actualizar la información de los estudiantes ya registrados.
+        </Text>
+        <Button
+          title="Ir a Inscripciones"
+          onPress={() => navigation.navigate("Inscripciones")}
+          color="#005187" // Color del botón
+        />
+      </View>
     </View>
   );
 }
@@ -44,5 +52,28 @@ const styles = StyleSheet.create({
     height: 300, // Alto de la imagen
     marginBottom: 20, // Espacio debajo de la imagen
     backgroundColor: "#4d82bc"
+  },
+  card: {
+    width: '80%',
+    backgroundColor: '#ffffff', // Color de fondo de la carta
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20, // Espacio debajo de la carta
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5, // Añadir sombra en Android
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10, // Espacio debajo del título
+  },
+  cardText: {
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 20, // Espacio debajo del texto
   },
 });
