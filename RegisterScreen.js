d29d02c8276e19de4e33bdf2f1aee97beac1a233
MaxFerrarottiDEV <maximo_ferrarotti@hotmail.com>
2024-10-16 16:31:40 -0300

    pequeño avance con RegisterScreen

--- RegisterScreen.js
+++ RegisterScreen.js
@@ -1,11 +1,20 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable prettier/prettier */ 
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default function RegisterScreen() {
+export default function RegisterScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modulo de inscripciones</Text>
+      {/* Barra superior con opciones */}
+      <View style={styles.header}>
+        <Text style={styles.headerText} onPress={() => navigation.navigate('Solicitudes')}>
+          Solicitudes
+        </Text>
+        <Text style={styles.headerText} onPress={() => navigation.navigate('Consultas')}>
+          Consultas
+        </Text>
+      </View>
+      <Text style={styles.title}>Módulo de Inscripciones</Text>
     </View>
   );
 }
@@ -13,12 +22,27 @@ export default function RegisterScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "start",
     alignItems: "center",
     backgroundColor: "#c4dafa",
+  },
+  header: {
+    flexDirection: 'row', // Coloca los textos en fila
+    justifyContent: 'space-around', // Espacio equitativo entre los textos
+    width: '100%', // Ancho completo
+    backgroundColor: '#ffffff', // Color de fondo blanco
+    paddingVertical: 15, // Espaciado vertical
+    borderBottomWidth: 1, // Borde inferior
+    borderBottomColor: '#cccccc', // Color del borde
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#005187', // Color del texto
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
+    justifyContent: "center",
   },
 });
