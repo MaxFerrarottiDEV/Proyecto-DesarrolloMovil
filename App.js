d0f5fecf758271805830a75a7d972d56ef1f4468
MaxFerrarottiDEV <maximo_ferrarotti@hotmail.com>
2024-10-16 15:39:37 -0300

    Avances para ProfileScreen y HomeScreen

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
-/* eslint-disable no-unused-expressions */
-import React from "react";
+import React, { useEffect } from "react";
+import { Text } from "react-native";
 import AppNavigator from "./AppNavigator";
+import { useFonts, Rubik_400Regular } from "@expo-google-fonts/rubik";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync(); // Prevenir que la pantalla de carga se oculte automáticamente
 
 export default function App() {
+  // Cargar la fuente Rubik
+  let [fontsLoaded] = useFonts({
+    Rubik_400Regular,
+  });
+
+  // Manejar la pantalla de carga
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync(); // Ocultar la pantalla de carga cuando las fuentes estén listas
+      }
+    };
+
+    hideSplashScreen();
+  }, [fontsLoaded]); // Ejecutar este efecto cuando las fuentes cambien
+
+  // Aplicar Rubik como fuente predeterminada en todos los Text
+  const defaultTextProps = Text.defaultProps || {};
+  defaultTextProps.style = { fontFamily: "Rubik_400Regular" };
+  Text.defaultProps = defaultTextProps;
+
+  // Retornar el AppNavigator
   return <AppNavigator />;
 }
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,10 +1,11 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable prettier/prettier */ 
 import React from "react";
-import { View, Text, Button, StyleSheet, Image } from "react-native";
+import { View, Text, Button, StyleSheet, Image, ScrollView } from "react-native";
 
+/* ScrollView envuelve todo el contenido */
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}> 
       <Text style={styles.title}>Instituto de Formación Docente</Text>
       <Text style={styles.subtitle}>Profesorado de Educación Especial con Orientación en Sordos e Hipoacúsicos</Text>
       
@@ -13,6 +14,13 @@ export default function HomeScreen({ navigation }) {
         source={require('./assets/Carrusel_1.jpg')} // Ruta de la imagen
         style={styles.image}  // Estilos para la imagen
       />
+      
+      {/* Misión de la organización */}
+      <Text style={styles.mision}>
+        “MEJORAR LA CALIDAD DE VIDA DE LAS PERSONAS CON DISCAPACIDAD AUDITIVA EN LA PROVINCIA DE SALTA A TRAVÉS DE UN SERVICIO INTEGRAL, 
+        BASADO EN LOS VALORES INSTITUCIONALES DE LIBERTAD, HONESTIDAD,
+        RESPETO, SOLIDARIDAD Y EFICIENCIA”
+      </Text>
       
       {/* Carta de presentación */}
       <View style={styles.card}>
@@ -26,32 +34,39 @@ export default function HomeScreen({ navigation }) {
           color="#005187" // Color del botón
         />
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#c4dafa",
+    paddingVertical: 20, // Añadir padding para evitar que el contenido esté pegado a los bordes
   },
   title: {
-    fontSize: 24,
+    fontSize: 26,
     fontWeight: "bold",
     marginBottom: 10, // Espacio entre el título y el subtítulo
   },
   subtitle: {
-    fontSize: 14,
+    fontSize: 18,
     fontWeight: "bold",
+    textAlign: "center",
     marginBottom: 20, // Espacio entre el subtítulo y la imagen
   },
   image: {
     width: 300,  // Ancho de la imagen
     height: 300, // Alto de la imagen
-    marginBottom: 20, // Espacio debajo de la imagen
+    marginBottom: 30, // Espacio debajo de la imagen
     backgroundColor: "#4d82bc"
+  },
+  mision: {
+    textAlign: "center",
+    marginBottom: 30,
+    fontStyle: "italic"
   },
   card: {
     width: '80%',
